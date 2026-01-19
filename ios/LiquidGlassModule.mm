20c24effb81d66ebb54f80ad576ecc8d29a742f2
raxodus <251626932+raxodus@users.noreply.github.com>
2026-01-19 18:59:47 +0100

    fix: isLiquidGlassSupported returning false

--- ios/LiquidGlassModule.mm
+++ ios/LiquidGlassModule.mm
@@ -1,42 +1,46 @@
 #import "LiquidGlassModule.h"
 
 @implementation LiquidGlassModule {
-  facebook::react::ModuleConstants<JS::NativeLiquidGlassModule::Constants> _constants;
+  facebook::react::ModuleConstants<JS::NativeLiquidGlassModule::Constants::Builder> _constants;
+  BOOL _constantsInitialized;
 }
 
-- (void)initialize
+RCT_EXPORT_MODULE(NativeLiquidGlassModule)
+
+- (void)initializeConstantsIfNeeded
 {
+  if (_constantsInitialized) {
+    return;
+  }
+  _constantsInitialized = YES;
+
 #if (defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 260000) || \
     (defined(__TV_OS_VERSION_MAX_ALLOWED) && __TV_OS_VERSION_MAX_ALLOWED >= 260000)
   if (@available(iOS 26.0, tvOS 26.0, *)) {
     NSDictionary *infoPlist = [[NSBundle mainBundle] infoDictionary];
     BOOL requiresDesignCompatibility = [infoPlist[@"UIDesignRequiresCompatibility"] boolValue];
-    
-    _constants = facebook::react::typedConstants<JS::NativeLiquidGlassModule::Constants>({
+
+    _constants = facebook::react::typedConstants<JS::NativeLiquidGlassModule::Constants::Builder>({
       .isLiquidGlassSupported = !requiresDesignCompatibility
     });
-    
+
     return;
   }
 #endif
-  
-  _constants = facebook::react::typedConstants<JS::NativeLiquidGlassModule::Constants>({
+
+  _constants = facebook::react::typedConstants<JS::NativeLiquidGlassModule::Constants::Builder>({
     .isLiquidGlassSupported = NO
   });
 }
 
-- (facebook::react::ModuleConstants<JS::NativeLiquidGlassModule::Constants>)constantsToExport
+- (facebook::react::ModuleConstants<JS::NativeLiquidGlassModule::Constants::Builder>)constantsToExport
 {
-  return (facebook::react::ModuleConstants<JS::NativeLiquidGlassModule::Constants>)[self getConstants];
+  return (facebook::react::ModuleConstants<JS::NativeLiquidGlassModule::Constants::Builder>)[self getConstants];
 }
 
-+ (NSString *)moduleName {
-  return @"NativeLiquidGlassModule";
-}
-
-
-- (facebook::react::ModuleConstants<JS::NativeLiquidGlassModule::Constants>)getConstants
+- (facebook::react::ModuleConstants<JS::NativeLiquidGlassModule::Constants::Builder>)getConstants
 {
+  [self initializeConstantsIfNeeded];
   return _constants;
 }
 
